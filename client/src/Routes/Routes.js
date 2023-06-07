4bd7bc7f0a49484b2035b5daedb1f6f0f9a23115
DihanAbir <nahid.muradabir@gmail.com>
2023-06-07 22:04:21 +0600

    added all file

---
+++ client/src/Routes/Routes.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Product from '../pages/Product/Product';
+import Main from '../layout/Main';
+import Dashboard from '../layout/Dashboard';
+import AddCard from '../pages/Profile/seller/AddCard';
+import MyProducts from '../pages/Profile/seller/MyProducts';
+import Login from '../pages/Login_Register/Login';
+import Register from '../pages/Login_Register/Register';
+import ProductDetails from '../components/ProductDetailsComponets/ProductDetails';
+import MyAccount from '../pages/DashboardPages/MyAccount';
+import MyOrders from '../pages/DashboardPages/MyOrders';
+import RecipientRegister from '../pages/DashboardPages/RecipientRegister';
+import SellerOrders from '../pages/DashboardPages/SellerOrders';
+import MyShoppingAddress from '../pages/DashboardPages/MyShoppingAddress';
+import BuyCard from '../pages/BuyCard/BuyCard';
+import StoreProfile from '../pages/StoreProfile/StoreProfile';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            { path: '/', element: <Home></Home> },
+            { path: '/home', element: <Home></Home> },
+            { path: '/products', element: <Product></Product> },
+            {
+                path: '/products/:id',
+                loader: ({ params }) => fetch(`http://localhost:5000/api/product/${params?.id}`),
+                element: <ProductDetails></ProductDetails>
+            },
+            {
+                path: '/products/:cardId/buy',
+                loader: ({ params }) => fetch(`http://localhost:5000/api/product/${params?.cardId}`),
+                element: <BuyCard></BuyCard>
+            },
+            {
+                path: '/stores/:username',
+                loader: ({ params }) => fetch(`http://localhost:5000/api/store/getInfo/${params?.username}`),
+                element: <StoreProfile></StoreProfile>
+            },
+
+            // store
+            { path: '/store/add-card', element: <AddCard></AddCard> },
+            { path: '/store/products', element: <MyProducts></MyProducts> },
+            { path: '/login', element: <Login></Login> },
+            { path: '/register', element: <Register></Register> },
+        ]
+    },
+    {
+        path: '/dashboard', element: <Dashboard />, children: [
+            { path: '/dashboard', element: <MyAccount /> },
+            { path: '/dashboard/my-account', element: <MyAccount /> },
+            { path: '/dashboard/orders', element: <MyOrders /> },
+            { path: '/dashboard/recipient', element: <RecipientRegister /> },
+            { path: '/dashboard/shopping-address', element: <MyShoppingAddress /> },
+            { path: '/dashboard/seller-orders', element: <SellerOrders /> },
+        ]
+    }
+])
+
+export default router;
